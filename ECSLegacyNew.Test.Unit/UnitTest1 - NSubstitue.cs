f3842d04f7344cd637445bbe2c7acc2d8ea234f6
Ditte Bertelsen <201907172@uni.au.dk>
2021-02-25 13:34:40 +0100

    Tilføjelse af testprogram som bruger NSubstitue. Testprogrammet indeholder udelukkende interactiontests.

---
+++ ECSLegacyNew.Test.Unit/UnitTest1 - NSubstitue.cs
@@ -0,0 +1,68 @@
+using System.Runtime.CompilerServices;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace ECSLegacy.Test.Unit
+{
+    public class Tests_NSubstitute
+    {
+        private ECS uut;
+        ITempSensor _fakeTempSensor;
+        IHeater _fakeHeater;
+
+        [SetUp]
+        public void Setup()
+        {
+            _fakeTempSensor = Substitute.For<ITempSensor>();
+            _fakeHeater = Substitute.For<IHeater>();
+            uut = new ECS(20, _fakeTempSensor,_fakeHeater);
+
+            //uut = new ECS(20, new FakeTempSensor(), new FakeHeater());
+        }
+
+        [TestCase(24)]
+        [TestCase(23)]
+        [TestCase(22)]
+        public void Regulate_TempOverThreshold_HeaterOff(int temp)
+        {
+            //Arrange 
+
+            //Tester metoden regulate at heateren turner off, når temperaturen ligger over tærskelværdien
+            //Ønsker at der skal være koldere
+
+            //Act
+
+            _fakeTempSensor.GetTemp().Returns(temp);
+            uut.Regulate();
+
+            //Assert
+            _fakeHeater.Received(1).TurnOff();
+
+        }
+
+        [TestCase(19)]
+        [TestCase(10)]
+        [TestCase(5)]
+        public void Regulate_TempunderThreshold_HeaterOn(int temp)
+        {
+            //Arrange 
+
+            //Tester metoden regulate at heateren turner on, når temperaturen ligger under tærskelværdien
+            //Ønsker at der skal være varmere
+
+            //Act
+
+            _fakeTempSensor.GetTemp().Returns(temp);
+            uut.Regulate();
+
+
+            //Assert
+
+            _fakeHeater.Received(1).TurnOn();
+
+        }
+
+    }
+
+
+}
